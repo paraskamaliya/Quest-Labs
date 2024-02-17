cd9eb6f8de97dd1aaaa0b800f1519dc6cfbe8a6d
Paras <paraskamaliya5@gmail.com>
2024-02-17 08:42:57 +0530

    feat: menubar updated

---
+++ src/Components/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Profile from '../Pages/Profile'
+
+function AllRoutes() {
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Profile />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default AllRoutes
---
+++ src/Components/MenuBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { RiHome6Fill } from "react-icons/ri";
+import { CiSearch } from "react-icons/ci";
+import { HiShoppingBag } from "react-icons/hi";
+import { IoMdPerson } from "react-icons/io";
+import { Link } from 'react-router-dom';
+
+function MenuBar() {
+    return (
+        <div className='menubar'>
+            <Link to={"#"} className='link'>
+                <RiHome6Fill className='icon' size={"1.5rem"} />
+            </Link>
+            <Link to={"#"} className='link'>
+                <CiSearch className='icon' size={"1.5rem"} />
+            </Link>
+            <Link to={"#"} className='link'>
+                <HiShoppingBag className='icon' size={"1.5rem"} />
+            </Link>
+            <Link to={"/"} className='link'>
+                <IoMdPerson className='icon' size={"1.5rem"} />
+            </Link>
+        </div>
+    )
+}
+
+export default MenuBar
